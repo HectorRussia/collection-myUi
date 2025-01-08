371cec952e91fb7d0c5171a53b7ca4fefcc5848f
Ponkrit <ponkrit.wo@hotmail.com>
2025-01-08 10:55:29 +0700

    chore : darkmode on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,42 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'yellow' : '#f5f242',
+      'black' : '#1c1c1b'
+    },
   },
   plugins: [
     require('daisyui')
   ],
+  daisyui: {
+    mythemes: [
+      {
+        light: {
+          "primary": "#4F46E5", 
+          "secondary": "#E11D48", 
+          "accent": "#FBBF24",
+        },
+      },
+      {
+        dark: {
+          "primary": "#292524", 
+          "secondary": "#EC4899",
+          "accent": "#F59E0B",
+        },
+      },
+    ],
+    themes: ["light", "dark"]
+  },
 }
 
